3c774c92a0168414658275f3455f759feef97325
Matthew Low <mattchrlw@gmail.com>
2024-11-02 05:31:16 +0000

    Support zero length subgraphs

--- graph.py
+++ graph.py
@@ -453,7 +453,10 @@ Define a subgraph. Take a 64 node deterministic BFS
 """
 def get_subgraph(G, root, length=64):
     edges = nx.bfs_edges(G, root)
-    nodes = ([root] + [v for u, v in edges])[:length]
+    if length != 0:
+        nodes = ([root] + [v for u, v in edges])[:length]
+    else:
+        nodes = [root] + [v for u, v in edges]
     return G.subgraph(nodes)
 
 if __name__ == "__main__":
